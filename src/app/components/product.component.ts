8ecd8243d38b2ffdee3253210a852e6af775d6c9
Sebastian Rozo <camynator@yahoo.es>
2021-05-17 22:32:20 -0500

    se agrega componente de productos junto a su modelo

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,11 +1,16 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductModel } from '../model/Product.model';
 
 @Component({
-    selector: 'app-product',
-    templateUrl:'./product.component.html',
-    
+  selector: 'app-product',
+  templateUrl: './product.component.html',
 })
-
 export class ProductComponent {
-
+  product: ProductModel = {
+    id: '1',
+    image: 'assets/camiseta.png',
+    title: 'Camiseta',
+    price: 80000,
+    description: 'bla bla bla',
+  };
 }
